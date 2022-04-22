08f5b570490dd0aa265ed70cad8a790a2ca85e62
IMDroidude <zareahmer89@gmail.com>
2022-04-22 16:30:28 +0500

    - implement Hilt to the application...

---
+++ app/src/main/java/uk/co/planetbeyond/meal_search_clean_architecture/App.kt
@@ -0,0 +1,12 @@
+package uk.co.planetbeyond.meal_search_clean_architecture
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
