008750433f05c0e0c4ba0a7d3cc6c8bf6067b651
IMDroidude <zareahmer89@gmail.com>
2022-05-18 16:23:40 +0500

    - Update binding in AppFragment.kt and AppActivity.kt...

--- app/src/main/java/uk/co/planetbeyond/meal_search_clean_architecture/App.kt
+++ app/src/main/java/uk/co/planetbeyond/meal_search_clean_architecture/App.kt
@@ -2,11 +2,17 @@ package uk.co.planetbeyond.meal_search_clean_architecture
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import uk.co.planetbeyond.data.prefs.AppPrefs
+import javax.inject.Inject
 
 @HiltAndroidApp
 class App : Application() {
 
+    ///@Inject lateinit var appPrefs: AppPrefs
+
     override fun onCreate() {
         super.onCreate()
+
+       ///appPrefs.isFirstTime()
     }
 }
